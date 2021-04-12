0db46f231598972e6c3dd904f159fb61b24a0125
Justin Deutsch <djustin8@vt.edu>
2021-04-12 17:10:18 -0400

    update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
 import setuptools
 
-setuptools.setup()
+install_requires = [
+    'gym==0.18.0',
+    'magent==0.1.13',
+    'PettingZoo==1.6.0',
+    'numpy==1.19.5',
+    'torch>=1.7.1',
+    'tensorboard==2.4.1',
+    'ray==1.2.0',
+    'seaborn==0.11.1',
+    'schedule==1.0.0',
+    'tqdm==4.59.0',
+]
+
+setuptools.setup(
+    name = 'muzero_collab',
+    version = '1.0.0',
+    author = "Alex Wilson, Justin Deutsch",
+    author_email = 'apw@vt.edu, djustin8@vt.edu',
+    description = 'Training collaborative MuZero agents in battle environment.',
+    install_requires=install_requires
+)
